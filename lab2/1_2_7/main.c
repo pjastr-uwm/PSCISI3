26c65f2f274d12739dec1bd2711665a5271ac76e
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-03-02 14:13:46 +0100

    2

---
+++ lab2/1_2_7/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int liczba;
+    scanf("%d", &liczba);
+    printf("%d\n", liczba+1);
+    return 0;
+}
---
+++ lab2/1_2_8/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    printf("%f\n", (a+b+c)/3.0);
+    return 0;
+}
