2ae44f6edabcf49a22f93192eced5f1555ee1987
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-03-09 14:26:03 +0100

    2_2_1

---
+++ lab3/2_2_1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int foo(int n)
+{
+    if (n>=0)
+        return n;
+    return -n;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", foo(n));
+    return 0;
+}
