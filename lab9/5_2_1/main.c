528adb417f167e2367150a71658cc38bcb184e60
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-04-20 14:34:50 +0200

    1

--- lab9/5_2_1/main.c
+++ lab9/5_2_1/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 void wyczysc(char*napis)
 {
@@ -7,10 +8,20 @@ void wyczysc(char*napis)
     // napis[0] = 0;
 }
 
+void wyczysc2(wchar_t*napis)
+{
+    napis[0] = '\0';
+}
+
 int main()
 {
+    setlocale(LC_ALL,"");
     char napis[] = "Olsztyn";
     wyczysc(napis);
     printf("%s.\n", napis);
+    wchar_t napis2[] = L"Olsztynąęół";
+    wprintf(L"%s.\n", napis2);
+    wyczysc2(napis2);
+    wprintf(L"%s.\n", napis2);
     return 0;
 }
