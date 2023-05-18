436a53c2ec55942ed6760e368256a04320c93d4e
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-05-18 14:56:59 +0200

    7_3_1+73_10

---
+++ lab13/7_3_1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct element
+{
+    int i;
+    struct element *next;
+};
+
+struct element * utworz()
+{
+    return NULL;
+};
+
+int main()
+{
+    printf("%p\n", utworz());
+    return 0;
+}
